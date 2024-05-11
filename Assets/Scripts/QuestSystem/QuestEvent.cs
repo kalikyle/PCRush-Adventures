f63d9b9035525c4ee2889f30addb7a8cbe4f7982
Kyleskie <pkyle232@gmail.com>
2024-05-12 00:05:12 +0800

    Added Quest System Placeholder

---
+++ Assets/Scripts/QuestSystem/QuestEvent.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class QuestEvent : MonoBehaviour
+{
+    public event Action<string> onStartQuest;
+
+    public void StartQuest(string id)
+    {
+        if(onStartQuest != null)
+        {
+            onStartQuest(id);
+        }
+    }
+
+    public event Action<string> onAdvanceQuest;
+
+    public void AdvanceQuest(string id)
+    {
+        if (onAdvanceQuest != null)
+        {
+            onAdvanceQuest(id);
+        }
+    }
+
+    public event Action<string> onFinishQuest;
+
+    public void FinishQuest(string id)
+    {
+        if (onFinishQuest != null)
+        {
+            onFinishQuest(id);
+        }
+    }
+
+    public event Action<Quest> onQuestStateChange;
+
+    public void QuestStateChange(Quest quest)
+    {
+        if (onQuestStateChange != null)
+        {
+            onQuestStateChange(quest);
+        }
+    }
+}
---
+++ Assets/Scripts/QuestSystem/QuestPoint.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class QuestPoint : MonoBehaviour
+{
+    private bool playerIsNear = false;
+
+
+    [Header("Quest")]
+    [SerializeField] private QuestInfoSO questinfoForPoint;
+
+    private string questId;
+
+    private QuestState currentQuestState;
+
+
+    [Header("Config")]
+    [SerializeField] private bool StartPoint = true;
+    [SerializeField] private bool finishPoint = true;
+
+    private void Awake()
+    {
+        questId = questinfoForPoint.id;
+    }
+
+    private void Start()
+    {
+        GameManager.instance.questEvents.onQuestStateChange += QuestStateChange;
+        
+    }
+
+    private void OnEnable()
+    {
+       
+    }
+
+    private void OnDisable()
+    {
+        GameManager.instance.questEvents.onQuestStateChange -= QuestStateChange;
+    }
+
+    private void startQuest()
+    {
+        if (!playerIsNear)
+        {
+            return;
+        }
+
+        if(currentQuestState.Equals(QuestState.CAN_START) && StartPoint)
+        {
+            GameManager.instance.questEvents.StartQuest(questId);
+        }
+        else if (currentQuestState.Equals(QuestState.CAN_FINISH) && finishPoint)
+        {
+            GameManager.instance.questEvents.FinishQuest(questId);
+        }
+
+
+        //GameManager.instance.questEvents.StartQuest(questId);
+        //GameManager.instance.questEvents.AdvanceQuest(questId);
+        //GameManager.instance.questEvents.FinishQuest(questId);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            startQuest();
+            //Debug.LogError("start");
+        }
+    }
+
+    private void QuestStateChange(Quest quest)
+    {
+        if(quest.info.id.Equals(questId))
+        {
+            currentQuestState = quest.state;
+           // Debug.Log("Quest with id:" + questId + "updated to state: " + currentQuestState);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            playerIsNear = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            playerIsNear = false;
+        }
+    }
+}
